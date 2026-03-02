9753bf20f7a947e052a16c347f45839550a0acbc
CasColucci <crpadgitt@gmail.com>
2026-03-02 08:07:10 -0500

    Adding joining lobby logic

--- server/Models/Game.cs
+++ server/Models/Game.cs
@@ -1,4 +1,4 @@
-namespace Models;
+namespace BracketGame.Server.Models;
 
 public class Game
 {
--- server/Models/Lobby.cs
+++ server/Models/Lobby.cs
@@ -1,10 +1,10 @@
-namespace Models;
+namespace BracketGame.Server.Models;
 
 public class Lobby
 {
     public Guid Id { get; set; }
-    public ICollection<Game> Games { get; set; }
-    public ICollection<Player> Players { get; set; }
+    public ICollection<Game> Games { get; set; } = new List<Game>();
+    public ICollection<Player> Players { get; set; } = new List<Player>();
     public string Code { get; set; }
     public Guid? ActiveGameId { get; set; }
 }
