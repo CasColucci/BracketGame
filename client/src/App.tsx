18225906636592310a3d79bbcfd2ab68138f4089
CasColucci <crpadgitt@gmail.com>
2026-03-02 21:16:28 -0500

    End to end is working!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [result, setResult] = useState<string>('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    async function createLobbyClick(displayName: string) {
+        try {
+            const response = await fetch('/api/lobby', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ displayName }),
+            })
+            const data = await response.json()
+            setResult(JSON.stringify(data))
+        } catch (err) {
+            console.error(err)
+            setResult('Something went wrong')
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={() => createLobbyClick("TestUser")}
+            >Test It</button>
+            <p>{result}</p>
+        </div >
+    )
 }
 
 export default App
